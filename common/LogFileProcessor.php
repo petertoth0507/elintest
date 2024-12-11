a433411b4d3deab9cc9a9a61f9e3dcebb81cb6cd
Péter Tóth <petertoth0507@gmail.com>
2024-12-11 22:33:34 +0100

    Tokeletes lett a parentid megallapitas

--- common/LogFileProcessor.php
+++ common/LogFileProcessor.php
@@ -21,6 +21,8 @@ class LogFileProcessor
     public $prevItem = [];
     public $currentItem = [];
 
+    public $parents = [['id' => '#', 'levelPos' => 0, 'parent' => '#']];
+
     public function readLogFile()
     {
         $logFilePath = (dirname(dirname(__FILE__)) . static::LOG_FILE_RELATIVE_PATH);
@@ -33,12 +35,22 @@ class LogFileProcessor
         while (!$logFile->eof()) {
             set_time_limit(60);
             $fileRow = $logFile->fgets();
-            if ($rowCounter > 1) {
+            if (strpos($fileRow, ' -> ')) {
                 $processedData = $this->processFileRow($fileRow);
-                if ($processedData['levelPos'] !== false) {
-                    $processedData += ['id' => $rowCounter];
-                    Yii::error(json_encode($processedData));
+                $processedData += ['id' => $rowCounter];
+                if (empty($this->prevItem)) {
+                    $this->prevItem = $processedData;
                 }
+                $this->setParentId($processedData);
+                if (end($this->parents)['levelPos'] === $processedData['levelPos']) {
+                    $processedData += ['parent' => end($this->parents)['parent']];
+                } else {
+                    $processedData += ['parent' => end($this->parents)['id']];
+                }
+
+                Yii::error(json_encode(end($this->parents)));
+                Yii::error(json_encode($processedData));
+                $this->prevItem = $processedData;
             }
             $rowCounter++;
             if ($rowCounter > static::LINE_LIMIT) {
@@ -49,7 +61,7 @@ class LogFileProcessor
     }
 
 
-    public function processFileRow(string $fileRow)
+    protected function processFileRow(string $fileRow)
     {
         $levelPos = strpos($fileRow, ' -> ');
         $trimmedRow = trim($fileRow);
@@ -63,10 +75,26 @@ class LogFileProcessor
 
         return [
             'levelPos' => $levelPos,
-            'startTime' => $startTime,
-            'usedMemory' => $usedMemory,
+            'startTime' => (float) $startTime,
+            'usedMemory' => (int) $usedMemory,
             'processedRow' => $processedRow,
             'logRow' => $logRow
         ];
+    }
+
+    protected function setParentId(array $currentData)
+    {
+        if ($currentData['levelPos'] > $this->prevItem['levelPos']) {
+            array_push($this->parents, ['id' => $this->prevItem['id'], 'levelPos' =>  $this->prevItem['levelPos'], 'parent' => $this->prevItem['parent']]);
+        } elseif ($currentData['levelPos'] < $this->prevItem['levelPos']) {
+            $whileNumber = (($this->prevItem['levelPos'] - $currentData['levelPos']) / 2) - 1;
+            while ($whileNumber > 0) {
+                array_pop($this->parents);
+                $whileNumber--;
+            }
+            while (end($this->parents)['levelPos'] !== $currentData['levelPos']) {
+                array_pop($this->parents);
+            }
+        }
     }
 }
